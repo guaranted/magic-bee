f4e12a36b224df1d97c886d6884b64cf4565223c
guaranted <73685531+guaranted@users.noreply.github.com>
2021-02-09 15:45:54 +0300

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,11 @@
+module.exports = {
+    emojis: {
+        off: '<:carpi:798449451403706378>  ',
+        error: '<:EngelliPng:708215766742204416> ',
+        queue: '<:ListePng:708215766754918512>  ',
+        music: '<:YoutubeLogoPng:708215767421812786> ',
+        success: '<:tik:798449162441588756>  ',
+    },
+  
+  filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
